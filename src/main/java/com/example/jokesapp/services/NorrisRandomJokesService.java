e94970f11cb73d64f0a008673694d8a3b0410229
mohamed <moh4med.ahmed@gmail.com>
2021-10-05 22:12:24 +0200

    refactor view

--- src/main/java/com/example/jokesapp/services/NorrisRandomJokesService.java
+++ src/main/java/com/example/jokesapp/services/NorrisRandomJokesService.java
@@ -1,17 +1,21 @@
 package com.example.jokesapp.services;
 
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
-import guru.springframework.norris.chuck.ChuckNorrisQuotes;
-import java.util.ArrayList;
-import java.util.Random;
 
 @Primary
 @Service
 public class NorrisRandomJokesService implements JokeService{
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public NorrisRandomJokesService() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
     @Override
     public String getJoke() {
 
-        return new ChuckNorrisQuotes().getRandomQuote();
+        return this.chuckNorrisQuotes.getRandomQuote();
     }
 }
